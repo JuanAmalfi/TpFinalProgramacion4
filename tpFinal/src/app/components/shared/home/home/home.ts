4a74981e4d6a9c9902ca48de7202d983720b0b8a
JuanAmalfi <juanamalfi1@gmail.com>
2025-11-20 17:00:36 -0300

    Mejore el Home, modifique para que todos los componentes tengan el mismo estilo e hice aun mas responsive el proyecto.

--- tpFinal/src/app/components/shared/home/home/home.ts
+++ tpFinal/src/app/components/shared/home/home/home.ts
@@ -17,7 +17,8 @@ export class Home {
   protected libros = this.store.libros;
 
   // ✅ Computed para libros destacados (ej: los primeros 4)
-  protected librosDestacados = computed(() => this.libros().slice(0, 4));
+ protected librosDestacados = computed(() => this.libros());
+
     constructor() {
     // ✅ Cargar libros automáticamente
     effect(() => {
@@ -27,7 +28,41 @@ export class Home {
 
 
   verDetalle(id: number | string) {
-    this.router.navigate(['/libro', id]);
+    this.router.navigate(['/libros', id]);
   }
+
+  novedades() {
+  return this.librosDestacados().slice(0, 8);
+}
+
+mejorCalificados() {
+  return [...this.librosDestacados()]
+    .sort((a, b) => (b.promedioCalificacion || 0) - (a.promedioCalificacion || 0))
+    .slice(0, 8);
+}
+
+recomendados() {
+  return this.librosDestacados().slice(4, 12);
+}
+redondear(valor: number | undefined | null): number {
+  return Math.round(valor ?? 0);
+}
+  // 👉 TOP 5
+  topCinco() {
+    return [...this.librosDestacados()]
+      .sort((a, b) => (b.promedioCalificacion || 0) - (a.promedioCalificacion || 0))
+      .slice(0, 5);
+  }
+
+
+  scrollLeft(id: string) {
+  const el = document.getElementById(id);
+  if (el) el.scrollBy({ left: -300, behavior: 'smooth' });
+}
+
+scrollRight(id: string) {
+  const el = document.getElementById(id);
+  if (el) el.scrollBy({ left: 300, behavior: 'smooth' });
+}
 
 }
